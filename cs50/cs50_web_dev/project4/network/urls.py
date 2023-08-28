0f52d054c92ccc811c715c89009063f5248904a7
Przekazmierczak <125646878+Przekazmierczak@users.noreply.github.com>
2023-08-28 18:31:55 +0000

    project4 final version

--- cs50/cs50_web_dev/project4/network/urls.py
+++ cs50/cs50_web_dev/project4/network/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path
 
 from . import views
@@ -7,5 +6,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("new_post", views.new_post, name="new_post"),
+    path("<int:user_id>", views.profile, name="profile"),
+    path("following", views.following, name="following"),
+
+    path("post/<int:post_id>", views.get_post, name="get_post"),
+    path("api/post/<int:post_id>/", views.update_post, name="update_post"),
 ]
