9c12f0b9ee14e4e2fbcfef1fdf7fae87f2303fcf
Przekazmierczak <125646878+Przekazmierczak@users.noreply.github.com>
2023-12-03 23:59:43 +0100

    update leetcode 23, 226

---
+++ leetcode/0023_Merge_k_Sorted_Lists2.py
@@ -0,0 +1,104 @@
+"""
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+Constraints:
+
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    # def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    def mergeKLists(self, lists):
+        def joinLists(list1, list2):
+            current = ListNode(0)
+            result = current
+
+            while list1 and list2:
+                if list1.val < list2.val:
+                    current.next = list1
+                    list1 = list1.next
+                else:
+                    current.next = list2
+                    list2 = list2.next
+                current = current.next
+
+            if not list1:
+                current.next = list2
+            elif not list2:
+                current.next = list1
+
+            return result.next
+        
+        def merge(left, right):
+            if left + 1 == right:
+                return joinLists(lists[left], lists[right])
+            if left == right:
+                return lists[left]
+            
+            mid = (left + right) // 2
+            return joinLists(merge(left, mid), merge(mid + 1, right))
+        
+        if not lists:
+            return None
+        
+        return merge(0, len(lists) - 1)
+
+
+def main():
+    head_list = [[1,4,5],[1,3,4],[2,6]]
+    lists = []
+    for element in head_list:
+        head = None
+        for i in reversed(range(len(element))):
+            head = ListNode(element[i], head)
+        lists.append(head)
+
+    solution = Solution()
+
+    result = solution.mergeKLists(lists)
+    
+    result_node_list = []
+    while result:
+        result_node_list.append(result.val)
+        result = result.next
+
+    print(result_node_list)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ leetcode/0226_Invert_Binary_Tree_Iterative.py
@@ -0,0 +1,96 @@
+"""
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+"""
+from collections import deque 
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    # def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    def invertTree(self, root):
+        stack = [root]
+        while stack:
+            node = stack.pop()
+
+            node.left, node.right = node.right, node.left
+            
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        return root
+
+
+def main():
+    null = None
+    root_list = [4,2,7,1,3,6,9]
+
+    def create_binary(root_list):
+        root = TreeNode(root_list[0])
+        node_list = [root]
+        i = 1
+
+        while i < len(root_list):
+            node = node_list.pop(0)
+
+            if root_list[i] != None:
+                node.left = TreeNode(root_list[i])
+                node_list.append(node.left)
+            i += 1
+
+            if i < len(root_list) and root_list[i] != None:
+                node.right = TreeNode(root_list[i])
+                node_list.append(node.right)
+            i += 1
+
+        return root
+
+    solution = Solution()
+
+    result = solution.invertTree(create_binary(root_list))
+
+    stack = deque([(result, 0)])
+    ans = []
+    
+    while stack:
+        node, level = stack.popleft()
+        
+        if node:           
+            ans.append(node.val)
+            stack.append((node.left, level + 1))
+            stack.append((node.right, level + 1))
+        else:
+            ans.append(None)
+    
+    while ans[-1] == None:
+        if ans[-1] == None:
+            ans.pop()
+
+    print(ans)
+
+if __name__ == "__main__":
+    main()
