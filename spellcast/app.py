0edcfc48a372b0724f4f7ea97c9701b5217c709b
Przekazmierczak <prze.kazmierczak@gmail.com>
2024-01-04 22:57:44 +0100

    count special blocks

--- spellcast/app.py
+++ spellcast/app.py
@@ -13,9 +13,9 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         trie_root = build_trie()
-        game_board = create_board()
+        game_board, special_block_board = create_boards()
         letters_array = flat_and_upper(game_board)
-        words_found = find_words(game_board, trie_root)
+        words_found = find_words(game_board, special_block_board, trie_root)
         answers = dict(sorted(words_found.items(), key=lambda item: item[1][1], reverse=True))
         post = True
         return render_template("index.html", answers=answers,
@@ -32,17 +32,22 @@ def flat_and_upper(game_array):
     flat_list = [x.upper() for x in flat_list]
     return flat_list
 
-def create_board():
+def create_boards():
     game_board = []
+    special_block_board = []
     counter = 1
     for _ in range(5):
         temp_array = []
+        temp_block_array = []
         for _ in range(5):
             letter = (request.form.get(f"{counter}")).lower()
+            special_block = (request.form.get(f"special_block_{counter}"))
             temp_array.append(letter)
+            temp_block_array.append(special_block)
             counter += 1
         game_board.append(temp_array)
-    return game_board
+        special_block_board.append(temp_block_array)
+    return game_board, special_block_board
 
 def build_trie():
     trie_root = Trie()
@@ -60,14 +65,14 @@ def build_trie():
                 curr_node.end = True
     return trie_root
 
-def find_words(array, trie):
+def find_words(game_board, special_block_board, trie):
     result = {}
     for row in range(5):
         for col in range(5):
-            check_position(row, col, trie, set(), [], array, result)
+            check_position(row, col, trie, set(), [], game_board, special_block_board, result)
     return result
 
-def check_position(row, col, node, visited, path, game_board, result):
+def check_position(row, col, node, visited, path, game_board, special_block_board, result):
     if row not in range(5) or col not in range(5) or (row, col) in visited or game_board[row][col] not in node.letters:
         return
     
@@ -77,16 +82,28 @@ def check_position(row, col, node, visited, path, game_board, result):
 
     if curr_node.end:
         letters_values = {"a":1, "b":4, "c":5, "d":3, "e":1, "f":5, "g":3, "h":4, "i":1, "j":7, "k":6, "l":3, "m":4, "n":2, "o":1, "p":4, "q":8, "r":2, "s":2, "t":2, "u":4, "v":5, "w":5, "x":7, "y":4, "z":8}
+        double = False
         word_list = []
         position_in_numbers = []
         value = 0
         for position in path:
             word_row, word_col = position
             letter = game_board[word_row][word_col]
+            special_block = special_block_board[word_row][word_col]
+            letter_value = letters_values[letter]
+            if special_block == 'double_letter':
+                letter_value *= 2
+            elif special_block == 'triple_letter':
+                letter_value *= 3
+            elif special_block == 'double_points':
+                double = True
             word_list.append(letter)
-            value += letters_values[letter]
+            value += letter_value
             position_in_numbers.append(word_row * 5 + word_col + 1)
         
+        if double:
+            value *= 2
+            
         if len(path) > 5:
             value += 10
 
@@ -101,7 +118,7 @@ def check_position(row, col, node, visited, path, game_board, result):
     
     for neighbour in neighbours:
         neighbour_row, neighbour_col = neighbour
-        check_position(neighbour_row, neighbour_col, curr_node, visited, path, game_board, result)
+        check_position(neighbour_row, neighbour_col, curr_node, visited, path, game_board, special_block_board, result)
     
     visited.remove((row, col))
     path.pop()
