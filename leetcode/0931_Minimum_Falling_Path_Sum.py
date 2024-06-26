f8770441018630504df97d5f0e66f7bbe38a9db6
Przekazmierczak <prze.kazmierczak@gmail.com>
2024-06-26 16:16:37 +0200

    update leetcode 931, 1155

---
+++ leetcode/0931_Minimum_Falling_Path_Sum.py
@@ -0,0 +1,52 @@
+"""
+Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+
+A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+
+Example 1:
+
+Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+Output: 13
+Explanation: There are two falling paths with a minimum sum as shown.
+
+Example 2:
+
+Input: matrix = [[-19,57],[-40,-5]]
+Output: -59
+Explanation: The falling path with a minimum sum is shown.
+
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 100
+-100 <= matrix[i][j] <= 100
+"""
+
+class Solution:
+    # def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+    def minFallingPathSum(self, matrix):
+        n = len(matrix)
+        for row in range(n - 2, -1, -1):
+            for col in range(n):
+                curr_min = matrix[row + 1][col]
+                if col - 1 >= 0:
+                    curr_min = min(matrix[row + 1][col - 1], curr_min)
+                if col + 1 < n:
+                    curr_min = min(matrix[row + 1][col + 1], curr_min)
+                matrix[row][col] += curr_min
+        
+        return min(matrix[0])
+            
+
+def main():
+    matrix = [[2,1,3],[6,5,4],[7,8,9]]
+
+    solution = Solution()
+
+    result = solution.minFallingPathSum(matrix)
+    
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
