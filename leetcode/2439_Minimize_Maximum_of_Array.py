419372696a17a284380c769588775672b840c859
Przekazmierczak <prze.kazmierczak@gmail.com>
2024-07-09 20:39:17 +0200

    update leetcode 1423, 2439

---
+++ leetcode/2439_Minimize_Maximum_of_Array.py
@@ -0,0 +1,60 @@
+"""
+You are given a 0-indexed array nums comprising of n non-negative integers.
+In one operation, you must:
+
+Choose an integer i such that 1 <= i < n and nums[i] > 0.
+Decrease nums[i] by 1.
+Increase nums[i - 1] by 1.
+Return the minimum possible value of the maximum integer of nums after performing any number of operations.
+
+Example 1:
+
+Input: nums = [3,7,1,6]
+Output: 5
+Explanation:
+One set of optimal operations is as follows:
+1. Choose i = 1, and nums becomes [4,6,1,6].
+2. Choose i = 3, and nums becomes [4,6,2,5].
+3. Choose i = 1, and nums becomes [5,5,2,5].
+The maximum integer of nums is 5. It can be shown that the maximum number cannot be less than 5.
+Therefore, we return 5.
+
+Example 2:
+
+Input: nums = [10,1]
+Output: 10
+Explanation:
+It is optimal to leave nums as is, and since 10 is the maximum value, we return 10.
+
+Constraints:
+
+n == nums.length
+2 <= n <= 105
+0 <= nums[i] <= 109
+"""
+import math
+
+class Solution:
+    # def minimizeArrayValue(self, nums: List[int]) -> int:
+    def minimizeArrayValue(self, nums):
+        curr_sum, res = 0, 0
+        
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+            res = max(res, math.ceil(curr_sum / (i + 1)))
+        
+        return res
+            
+
+def main():
+    nums = [3,7,1,6]
+
+    solution = Solution()
+
+    result = solution.minimizeArrayValue(nums)
+    
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
