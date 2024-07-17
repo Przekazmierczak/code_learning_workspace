695533e96014c89a46a3a31e2bec6f26ff194fde
Przekazmierczak <prze.kazmierczak@gmail.com>
2024-07-17 19:50:42 +0200

    update leetcode 452, 1523, 1572

---
+++ leetcode/1523_Count_Odd_Numbers_in_an_Interval_Range.py
@@ -0,0 +1,42 @@
+"""
+Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+Example 1:
+
+Input: low = 3, high = 7
+Output: 3
+Explanation: The odd numbers between 3 and 7 are [3,5,7].
+
+Example 2:
+
+Input: low = 8, high = 10
+Output: 1
+Explanation: The odd numbers between 8 and 10 are [9].
+
+Constraints:
+
+0 <= low <= high <= 10^9
+"""
+
+class Solution:
+    # def countOdds(self, low: int, high: int) -> int:
+    def countOdds(self, low, high):
+        num = (high - low) // 2
+        if low % 2 or high % 2:
+            num += 1
+        return num
+            
+
+def main():
+    low = 8
+    high = 10
+
+    solution = Solution()
+
+    result = solution.countOdds(low, high)
+    
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
