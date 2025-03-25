9be3626a9305a16e7c0ecb3ab546f505acd90c5d
Przekazmierczak <prze.kazmierczak@gmail.com>
2025-03-25 22:56:57 +0100

    Add new board creation logic

--- study/c++/chess/pieces.cpp
+++ study/c++/chess/pieces.cpp
@@ -3,21 +3,41 @@
 #include <vector>
 #include <array>
 #include <tuple>
+#include <unordered_map>
+#include <memory>
 
 class Piece {
     public:
         const int ROWS = 8;
         const int COLS = 8;
-        std::string piece;
-        std::string player;
+        char symbol;
         int row;
         int column;
+        std::string piece;
+        std::string player;
 
-        Piece(std::string input_piece, std::string input_player, int input_row, int input_column) {
-            piece = input_piece;
-            player = input_player;
+        std::unordered_map<char, std::unordered_map<std::string, std::string>> legend = {
+            {'R', {{"piece", "rook"}, {"player", "white"}}},
+            {'N', {{"piece", "knight"}, {"player", "white"}}},
+            {'B', {{"piece", "bishop"}, {"player", "white"}}},
+            {'K', {{"piece", "king"}, {"player", "white"}}},
+            {'Q', {{"piece", "queen"}, {"player", "white"}}},
+            {'P', {{"piece", "pawn"}, {"player", "white"}}},
+            {'r', {{"piece", "rook"}, {"player", "black"}}},
+            {'n', {{"piece", "knight"}, {"player", "black"}}},
+            {'b', {{"piece", "bishop"}, {"player", "black"}}},
+            {'k', {{"piece", "king"}, {"player", "black"}}},
+            {'q', {{"piece", "queen"}, {"player", "black"}}},
+            {'p', {{"piece", "pawn"}, {"player", "black"}}}
+        };
+
+        Piece(char input_symbol, int input_row, int input_column) {
+            symbol = input_symbol;
             row = input_row;
             column = input_column;
+
+            auto piece = legend[input_symbol]["piece"];
+            auto player = legend[input_symbol]["player"];
         }
 
         struct Result {
@@ -26,78 +46,121 @@ class Piece {
             bool promotion = false;
         };
 
-        bool is_valid_position (int row, int column) {
+        bool is_valid_position(int row, int column) {
             return row >= 0 && row < ROWS && column >= 0 && column < COLS;
         }
 
-        Result check_piece_possible_moves (std::vector<std::tuple<std::string, std::string, std::array<int, 2>>> board, std::string turn) {
-            Result result;
+        // Result check_piece_possible_moves (std::vector<std::tuple<std::string, std::string, std::array<int, 2>>> board, std::string turn) {
+        //     Result result;
 
-            bool opponent = true;
+        //     bool opponent = true;
 
-            if (piece == "Pawn") {
-                int direction_by_colour = player == "white" ? 1: -1;
+        //     if (piece == "Pawn") {
+        //         int direction_by_colour = player == "white" ? 1: -1;
 
-                std::vector<std::array<int, 2>> directions = {{direction_by_colour, 0}};
+        //         std::vector<std::array<int, 2>> directions = {{direction_by_colour, 0}};
 
-                if ((player == "white" && row == 1) || (player == "black" && row == 6)) {
-                    directions.push_back({direction_by_colour * 2, 0});
-                }
+        //         if ((player == "white" && row == 1) || (player == "black" && row == 6)) {
+        //             directions.push_back({direction_by_colour * 2, 0});
+        //         }
 
-                if ((player == "white" && row == 6) || (player == "black" && row == 1)){
-                    result.promotion = true;
-                }
+        //         if ((player == "white" && row == 6) || (player == "black" && row == 1)){
+        //             result.promotion = true;
+        //         }
 
-                if (opponent) {
-                    directions = {{direction_by_colour, 1}, {direction_by_colour, -1}};
-                    for (auto direction: directions) {
-                        int new_row = row + direction[0];
-                        int new_column = row + direction[1];
+        //         if (opponent) {
+        //             directions = {{direction_by_colour, 1}, {direction_by_colour, -1}};
+        //             for (auto direction: directions) {
+        //                 int new_row = row + direction[0];
+        //                 int new_column = row + direction[1];
 
-                    }
-                }
-
-
+        //             }
+        //         }
 
 
 
-                for (auto direction : directions) {
-                    std::cout << direction[0] << "\n";
-                    std::cout << direction[1] << "\n";
-                }
 
-                std::cout <<"promotion:" << result.promotion << "\n";
-            }
 
-            return result;
-        }
+        //         for (auto direction : directions) {
+        //             std::cout << direction[0] << "\n";
+        //             std::cout << direction[1] << "\n";
+        //         }
+
+        //         std::cout <<"promotion:" << result.promotion << "\n";
+        //     }
+
+        //     return result;
+        // }
 };
 
 class Board {
     public:
         const int ROWS = 8;
         const int COLS = 8;
+        
         std::string turn;
         std::string castling;
-        std::string enpassant;
+        std::array<int, 2> enpassant;
+        std::array<std::array<std::unique_ptr<Piece>, 8>, 8> board;
+
+        std::array<std::array<std::unique_ptr<Piece>, 8>, 8> create_board () {
+            std::array<std::array<char, 8>, 8> simplify_board = {{
+                {'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
+                {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                {'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r'}
+            }};
+
+            std::array<std::array<std::unique_ptr<Piece>, 8>, 8> board;
+
+            for (int row = 0; row < ROWS; row++) {
+                for (int col = 0; col < COLS; col++) {
+                    if (simplify_board[row][col] != ' ') {
+                        board[row][col] = std::make_unique<Piece>(simplify_board[row][col], row, col);
+                    }
+                }
+            }
+
+            return board;
+        }
+        // std::array<std::array<Piece, 8>, 8> create_board (std::array<std::array<char, 8>, 8>simplify_board) {
+
+        // }
         
+        Board() {
+            turn = "white";
+            castling = "KQkq";
+            enpassant = {NULL, NULL};
+            board = create_board ();
+        }
 };
 
 
 
 int main() {
-    Piece piece("Pawn", "white", 1, 3);
+    // Piece piece("Pawn", "white", 1, 3);
 
-    std::vector<std::tuple<std::string, std::string, std::array<int, 2>>> board = {
-        {"pawn", "white", {1, 0}}, {"pawn", "white", {2, 4}}, {"pawn", "white", {1, 6}},
-        {"pawn", "black", {6, 3}}, {"pawn", "black", {5, 4}}, {"pawn", "black", {6, 7}},
-        {"rook", "white", {0, 0}}, {"rook", "white", {5, 7}}, {"rook", "black", {7, 0}}, {"rook", "black", {7, 4}},
-        {"knight", "white", {2, 0}}, {"knight", "white", {6, 4}}, {"knight", "black", {7, 1}}, {"knight", "black", {5, 5}},
-        {"bishop", "white", {4, 1}}, {"bishop", "black", {7, 2}}, {"queen", "white", {4, 3}},
-        {"king", "white", {0, 4}}, {"king", "black", {7, 3}}
-    };
+    // std::vector<std::tuple<std::string, std::string, std::array<int, 2>>> board = {
+    //     {"pawn", "white", {1, 0}}, {"pawn", "white", {2, 4}}, {"pawn", "white", {1, 6}},
+    //     {"pawn", "black", {6, 3}}, {"pawn", "black", {5, 4}}, {"pawn", "black", {6, 7}},
+    //     {"rook", "white", {0, 0}}, {"rook", "white", {5, 7}}, {"rook", "black", {7, 0}}, {"rook", "black", {7, 4}},
+    //     {"knight", "white", {2, 0}}, {"knight", "white", {6, 4}}, {"knight", "black", {7, 1}}, {"knight", "black", {5, 5}},
+    //     {"bishop", "white", {4, 1}}, {"bishop", "black", {7, 2}}, {"queen", "white", {4, 3}},
+    //     {"king", "white", {0, 4}}, {"king", "black", {7, 3}}
+    // };
 
-    Piece::Result result = piece.check_piece_possible_moves(board, "white");
+    // Piece::Result result = piece.check_piece_possible_moves(board, "white");
+
+    Board class_board;
+
+    auto board = class_board.create_board();
+
+    std::cout << board[0][3]->piece << std::endl;
+    std::cout << class_board.turn << std::endl;
 
     return 0;
 }
