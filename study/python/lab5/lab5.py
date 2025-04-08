fb822c2ec8dfc89d60a8094f8278b4cd9dcad145
Przekazmierczak <prze.kazmierczak@gmail.com>
2025-04-08 21:35:08 +0200

    Fix lab5.py add tests

--- study/python/lab5/lab5.py
+++ study/python/lab5/lab5.py
@@ -1,118 +1,255 @@
 import math
 
-class Lab1:
-    def __init__(self):
-        self.tasks = [
-            self.task1
-            ]
+class RationalNum:
+    """
+    Opracuj moduł, który będzie definiował klasę, reprezentującą liczby wymierne dodatnie
+    w postaci liczby całkowitej i ułamka właściwego.
 
-    def task1(self):
-        class RationalNum:
-            def __init__(self, intig, numerator, denominator):
-                self.input_intig = intig
-                self.input_numerator = numerator
-                self.input_denominator = denominator
-                self.numerator, self.denominator = self.reduce()
+    Obiekt powinien być tworzony na podstawie trzech informacji: wartości całkowitej
+    liczby, licznika części ułamkowej i mianownika części ułamkowej. W obiekcie informacje
+    o liczbie powinny być reprezentowane za pomocą liczb całkowitych int.
+    """
+    def __init__(self, whole, numerator=0, denominator=0):
+        # RationalNum(x) create whole numer x
+        if not numerator and not denominator:
+            whole -= 1
+            numerator, denominator = 1, 1
 
-            def reduce(self):
-                numerator = self.input_numerator + self.input_intig * self.input_denominator
-                common_deno = math.gcd(numerator, self.input_denominator)
-                numerator //= common_deno
-                denominator = self.input_denominator // common_deno
-                return numerator, denominator
+        # RationalNum(x, y) create x/y
+        elif not denominator:
+            whole, numerator, denominator = 0, whole, numerator
 
-            def __str__(self):
-                return f"{self.input_intig} {self.input_numerator}/{self.input_denominator}"
-            
-            def print(self):
-                return f"{self.numerator}/{self.denominator}"
-            
-            def __repr__(self):
-                return f"Rational number: {self.numerator} / {self.denominator}"
-            
-            def __eq__(self, other):
-                return self.numerator == other.numerator and self.denominator == other.denominator
-            
-            def __lt__(self, other):
-                return self.numerator * other.denominator < other.numerator * self.denominator
-            
-            def __le__(self, other):
-                return self.numerator * other.denominator <= other.numerator * self.denominator
-            
-            def __gt__(self, other):
-                return self.numerator * other.denominator > other.numerator * self.denominator
-            
-            def __ge__(self, other):
-                return self.numerator * other.denominator >= other.numerator * self.denominator
-            
-            def __add__(self, other):
-                if (isinstance(other, RationalNum)):
-                    add_numerator = self.numerator * other.denominator + self.denominator * self.numerator
-                    add_denominator = self.denominator * other.denominator
-                else:
-                    add_numerator = self.numerator + int(other) * self.denominator
-                    add_denominator = self.denominator
-                return RationalNum(0, add_numerator, add_denominator)
-            
-            def __mul__(self, other):
-                if (isinstance(other, RationalNum)):
-                    mull_numerator = self.numerator * other.numerator
-                    mull_denominator = self.denominator * other.denominator
-                else:
-                    mull_numerator = self.numerator * int(other)
-                    mull_denominator = self.denominator
-                return RationalNum(0, mull_numerator, mull_denominator)
-            
-            def __truediv__(self, other):
-                if (isinstance(other, RationalNum)):
-                    truediv_numerator = self.numerator * other.denominator
-                    truediv_denominator = self.denominator * other.numerator
-                else:
-                    truediv_numerator = self.numerator
-                    truediv_denominator = self.denominator * int(other)
-                return RationalNum(0, truediv_numerator, truediv_denominator)
-            
-            def __pow__(self, other):
-                pow_numerator = self.numerator ** other
-                pow_denominator = self.denominator ** other
-                return RationalNum(0, pow_numerator, pow_denominator)
-            
-            def __bool__(self):
-                return True if self.numerator != 0 else False
-            
-            def __float__(self):
-                return self.numerator / self.denominator
+        self.numerator, self.denominator = self.change_to_rational(whole, numerator, denominator)
 
-            def __int__(self):
-                return int(self.numerator / self.denominator)
-                
+    def change_to_rational(self, whole, numerator, denominator):
+        numerator = numerator + whole * denominator
+        gcd = math.gcd(numerator, denominator)
+        numerator //= gcd
+        denominator = denominator // gcd
+        return numerator, denominator
 
-        num = RationalNum(2, 3, 6)
-        other = RationalNum(2, 1, 3)
-        other2 = RationalNum(0, 5, 2)
-        print(num)
-        num.print()
-        assert bool(num) == True
-        assert bool(RationalNum(0, 0, 5) == False)
-        print(int(num))
+    def __str__(self):
+        """
+        Wyświetl liczbę w postaci ułamka właściwego.
+        """
+        whole = self.numerator // self.denominator
+        numerator = self.numerator % self.denominator
 
-
-
-    def call_task(self, i):
-        if 0 <= i < len(self.tasks):
-            self.tasks[i]()
+        if whole and numerator:
+            return f"{whole} {numerator}/{self.denominator}"
+        elif whole:
+            return f"{whole}"
+        elif numerator:
+            return f"{numerator}/{self.denominator}"
         else:
-            print(f"Pick number from 0 to {len(self.tasks)}!")
+            return f"0"
+    
+    def improper_fraction(self):
+        """
+        Zwróć tekst z liczbą w postaci ułamka niewłaściwego.
+        """
+        return f"{self.numerator}/{self.denominator}" if self.denominator != 1 else f"{self.numerator}"
+    
+    def __repr__(self):
+        """
+        Zwróć formę reprezentacyjną.
+        """
+        return f"Rational number: {self.improper_fraction()}"
+    
+    def __eq__(self, other):
+        """
+        Porównnaj liczby "==".
+        """
+        return self.numerator == other.numerator and self.denominator == other.denominator
+    
+    def __lt__(self, other):
+        """
+        Porównnaj liczby "<".
+        """
+        return self.numerator * other.denominator < other.numerator * self.denominator
+    
+    def __le__(self, other):
+        """
+        Porównnaj liczby "<=".
+        """
+        return self.numerator * other.denominator <= other.numerator * self.denominator
+    
+    def __gt__(self, other):
+        """
+        Porównnaj liczby ">".
+        """
+        return self.numerator * other.denominator > other.numerator * self.denominator
+    
+    def __ge__(self, other):
+        """
+        Porównnaj liczby ">=".
+        """
+        return self.numerator * other.denominator >= other.numerator * self.denominator
+    
+    def __add__(self, other):
+        """
+        Zwróć sumę "+".
+        """
+        if (isinstance(other, int)):
+            other = RationalNum(other)
 
+        add_numerator = self.numerator * other.denominator + self.denominator * other.numerator
+        add_denominator = self.denominator * other.denominator
 
+        return RationalNum(add_numerator, add_denominator)
+    
+    def __mul__(self, other):
+        """
+        Zwróć iloczyn "*".
+        """
+        if (isinstance(other, int)):
+            other = RationalNum(other)
+
+        mul_numerator = self.numerator * other.numerator
+        mul_denominator = self.denominator * other.denominator
+        return RationalNum(mul_numerator, mul_denominator)
+    
+    def __truediv__(self, other):
+        """
+        Zwróć iloraz "/".
+        """
+        if (isinstance(other, RationalNum)):
+            truediv_numerator = self.numerator * other.denominator
+            truediv_denominator = self.denominator * other.numerator
+        else:
+            truediv_numerator = self.numerator
+            truediv_denominator = self.denominator * int(other)
+        return RationalNum(0, truediv_numerator, truediv_denominator)
+    
+    def __pow__(self, other):
+        """
+        Zwróć potegę "**".
+        """
+        pow_numerator = self.numerator ** other
+        pow_denominator = self.denominator ** other
+        return RationalNum(0, pow_numerator, pow_denominator)
+    
+    def __bool__(self):
+        """
+        Konwertuj do bool "bool()".
+        """
+        return True if self.numerator != 0 else False
+    
+    def __float__(self):
+        """
+        Konwertuj do float "float()".
+        """
+        return self.numerator / self.denominator
+
+    def __int__(self):
+        """
+        Konwertuj do int "int()".
+        """
+        return int(self.numerator / self.denominator)
+        
 if __name__ == "__main__":
-    lab1 = Lab1()
-    while True:
-        try:
-            task = int(input("Pick a task (0 to exit): "))
-            if task:
-                lab1.call_task(task - 1) 
-            else:
-                break
-        except ValueError:
-            break
+    num1 = RationalNum(2, 3, 6)
+    num2 = RationalNum(2, 1, 2)
+    num3 = RationalNum(0, 4, 5)
+    num4 = RationalNum(8, 0, 3)
+    num5 = RationalNum(1, 4, 1)
+    num6 = RationalNum(8)
+    num7 = RationalNum(1, 4)
+
+    "TEST __str__()"
+    assert num1.__str__() == "2 1/2"
+    assert num2.__str__() == "2 1/2"
+    assert num3.__str__() == "4/5"
+    assert num4.__str__() == "8"
+    assert num5.__str__() == "5"
+    assert num6.__str__() == "8"
+    assert num7.__str__() == "1/4"
+
+    "TEST improper_fraction()"
+    assert num1.improper_fraction() == "5/2"
+    assert num2.improper_fraction() == "5/2"
+    assert num3.improper_fraction() == "4/5"
+    assert num4.improper_fraction() == "8"
+    assert num5.improper_fraction() == "5"
+    assert num6.improper_fraction() == "8"
+    assert num7.improper_fraction() == "1/4"
+
+    "TEST __repr__()"
+    assert num1.__repr__() == "Rational number: 5/2"
+    assert num2.__repr__() == "Rational number: 5/2"
+    assert num3.__repr__() == "Rational number: 4/5"
+    assert num4.__repr__() == "Rational number: 8"
+    assert num5.__repr__() == "Rational number: 5"
+    assert num6.__repr__() == "Rational number: 8"
+    assert num7.__repr__() == "Rational number: 1/4"
+
+    "TEST __eq__()"
+    assert num1 == num2
+    assert not num1 == num3
+    assert not num1 == num4
+    assert not num1 == num5
+    assert not num3 == num4
+    assert not num4 == num5
+    assert num4 == num6
+    assert not num4 == num7
+
+    "TEST __lt__()"
+    assert not num1 < num2
+    assert not num1 < num3
+    assert num1 < num4
+    assert num1 < num5
+    assert num3 < num4
+    assert not num4 < num5
+    assert not num4 < num6
+    assert not num4 < num7
+
+    "TEST __le__()"
+    assert num1 <= num2
+    assert not num1 <= num3
+    assert num1 <= num4
+    assert num1 <= num5
+    assert num3 <= num4
+    assert not num4 <= num5
+    assert num4 <= num6
+    assert not num4 <= num7
+
+    "TEST __gt__()"
+    assert not num1 > num2
+    assert num1 > num3
+    assert not num1 > num4
+    assert not num1 > num5
+    assert not num3 > num4
+    assert num4 > num5
+    assert not num4 > num6
+    assert num4 > num7
+
+    "TEST __ge__()"
+    assert num1 >= num2
+    assert num1 >= num3
+    assert not num1 >= num4
+    assert not num1 >= num5
+    assert not num3 >= num4
+    assert num4 >= num5
+    assert num4 >= num6
+    assert num4 >= num7
+
+    "TEST __add__()"
+    assert num1 + num2 == RationalNum(5)
+    assert num1 + num2 == 5
+    assert num1 + num3 == RationalNum(33, 10)
+    assert num1 + num4 == RationalNum(10, 1, 2)
+    assert num1 + num5 == RationalNum(7, 1, 2)
+    assert num3 + num4 == RationalNum(8, 4, 5)
+    assert num4 + num5 == 13
+    assert num1 + 3 == RationalNum(3, 5, 2)
+
+    "TEST __mul__()"
+    assert num1 * num2 == RationalNum(25, 4)
+    assert num1 * num3 == RationalNum(2)
+    assert num1 * num3 == 2
+    assert num1 * num4 == 20
+    assert num1 * num5 == RationalNum(25, 2)
+    assert num3 * num4 == RationalNum(32, 5)
+    assert num4 * num5 == 40
+
+    print("Finished all tests.")
